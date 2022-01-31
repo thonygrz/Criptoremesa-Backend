0861575feba22b88fc69e5e54f643778a09d70fc
Anthony Rodriguez <anthony81199@gmail.com>
2022-01-31 19:46:12 -0400

    cambios

--- src/modules/chat/services/chat.service.js
+++ src/modules/chat/services/chat.service.js
@@ -160,6 +160,7 @@ chatService.getMessages = async (req, res, next,email_user) => {
       logger.info(`[${context}]: Getting all messages from chat`);
       ObjLog.log(`[${context}]: Getting all messages from chat`);
       data = await chatPGRepository.getMessages(email_user);
+      console.log('data: ',data)
       data.forEach((el) => {
         if (el.file !== 'null' && el.type === 'image') {
           el.file = fs.readFileSync(el.file);
