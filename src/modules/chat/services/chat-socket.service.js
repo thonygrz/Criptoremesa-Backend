bc8904b1f346c8b91d4db502a550e5276c2ba5e9
Anthony Rodriguez <anthony81199@gmail.com>
2022-02-09 17:04:32 -0400

    implementando mmmagic

--- src/modules/chat/services/chat-socket.service.js
+++ src/modules/chat/services/chat-socket.service.js
@@ -1,12 +1,19 @@
 import chatPGRepository from "../repositories/chat.pg.repository";
-import {fileTypeFromBuffer} from 'file-type';
-
+// import {fileTypeFromBuffer} from 'file-type';
+var Magic = require('mmmagic').Magic;
 const chaSocketService = {};
 
 chaSocketService.sendMessage = async (body) => {
   try {
     if (body.file) {
-        console.log('BCUWIBCHJWEBKCJXW',await fileTypeFromBuffer(body.file));
+        var magic = new Magic();
+  
+            magic.detect(body.file, function(err, result) {
+                if (err) throw err;
+                console.log(result);
+                // output: Python script, ASCII text executable
+            });
+        // console.log('BCUWIBCHJWEBKCJXW',await fileTypeFromBuffer(body.file));
     }  
     // await chatPGRepository.sendMessage(body);
   } catch (error) {
