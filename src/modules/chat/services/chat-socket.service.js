de7176c246d91f8644df59a008232e3e0a8db3bc
Anthony Rodriguez <anthony81199@gmail.com>
2022-02-09 18:18:20 -0400

    function

--- src/modules/chat/services/chat-socket.service.js
+++ src/modules/chat/services/chat-socket.service.js
@@ -4,6 +4,12 @@ import {env} from '../../../utils/enviroment'
 
 const chatSocketService = {};
 
+function between(min, max) {  
+  return Math.floor(
+    Math.random() * (max - min + 1) + min
+  )
+}
+
 chatSocketService.sendMessage = async (body) => {
   try {
     if (body.file) {
