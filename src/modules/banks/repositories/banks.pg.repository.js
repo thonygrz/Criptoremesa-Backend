64f5d82e89d4aff3e02589f431ae0bfc79412e92
Anthony Rodriguez <anthony81199@gmail.com>
2022-02-22 21:06:09 -0400

    cambio en la obtencion de paises de origen

--- src/modules/banks/repositories/banks.pg.repository.js
+++ src/modules/banks/repositories/banks.pg.repository.js
@@ -13,7 +13,7 @@ banksRepository.getOriginBanks = async (countryId,payMethodId,currencyId) => {
     ObjLog.log(`[${context}]: Getting origin banks from db`);
     await pool.query("SET SCHEMA 'msg_app'");
     const resp = await pool.query(
-      `select * from sp_ms_banks_get_origin_banks('{${countryId}}','{${currencyId}}')`
+      `select * from sp_get_origin_banks_by_country_and_currency('{${countryId}}','{${currencyId}}')`
     );
     return resp.rows;
   } catch (error) {
