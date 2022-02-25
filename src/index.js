c3386cdbb464640c7c58dc2f0b7bbfe8ea100316
Anthony Rodriguez <anthony81199@gmail.com>
2022-02-25 17:09:45 -0400

    cambio a https

--- src/index.js
+++ src/index.js
@@ -9,24 +9,24 @@ import { SocketServer } from "./modules/sockets/sockets.coordinator";
 // Para usar el certificado
 ///////////////////////////
 
-// const https = require("https");
+const https = require("https");
 
-// const fs = require("fs");
+const fs = require("fs");
 
-// const httpsServer = https.createServer(
-//   {
-//     key: fs.readFileSync(path.join(__dirname, "utils", "cert", "criptoremesa.key")),
-//     cert: fs.readFileSync(path.join(__dirname, "utils", "cert", "criptoremesa.cert")),
-//     requestCert: false,
-//     rejectUnauthorized: false
-//   },
-//   app
-// );
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(__dirname, "utils", "cert", "criptoremesa.key")),
+    cert: fs.readFileSync(path.join(__dirname, "utils", "cert", "criptoremesa.cert")),
+    requestCert: false,
+    rejectUnauthorized: false
+  },
+  app
+);
 
-// httpsServer.listen(app.get("port"), "0.0.0.0", () => {
-//   logger.info(`Server on port ${app.get("port")}`);
-//   ObjLog.log(`Server on port ${app.get("port")}`);
-// });
+httpsServer.listen(app.get("port"), "0.0.0.0", () => {
+  logger.info(`Server on port ${app.get("port")}`);
+  ObjLog.log(`Server on port ${app.get("port")}`);
+});
 
 ////////////////////////////////
 // Sin el certificado - forma 1
@@ -45,10 +45,10 @@ import { SocketServer } from "./modules/sockets/sockets.coordinator";
 // Sin el certificado - forma 2
 ///////////////////////////////
 
-const server = app.listen(app.get("port"), () => {
-  logger.info(`Server on port ${app.get("port")}`);
-  ObjLog.log(`Server on port ${app.get("port")}`);
-});
+// const server = app.listen(app.get("port"), () => {
+//   logger.info(`Server on port ${app.get("port")}`);
+//   ObjLog.log(`Server on port ${app.get("port")}`);
+// });
 
 console.log(app.get('env'))
 
