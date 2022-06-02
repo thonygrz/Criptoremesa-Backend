399d5527afd365d9be080af08f3b012f7be50058
thony <anthony81199@gmail.com>
2022-06-02 16:22:58 -0400

    se corrige error en servicio

--- src/modules/all_countries/services/all_countries.service.js
+++ src/modules/all_countries/services/all_countries.service.js
@@ -9,6 +9,7 @@ all_countriesService.getall_countries = async (req, res, next) => {
   try {
     logger.info(`[${context}]: Searching in DB`);
     ObjLog.log(`[${context}]: Searching in DB`);
+    let data
     data = await all_countriesPGRepository.getall_countries();
     res.status(200).json(data);
   } catch (error) {
