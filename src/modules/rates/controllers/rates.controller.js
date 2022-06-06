a35eca3aa920cf8acd9849a8b31aaaff8f57b234
thony <anthony81199@gmail.com>
2022-06-06 08:06:14 -0400

    se protegen rutas y se corrige código de /rates

--- src/modules/rates/controllers/rates.controller.js
+++ src/modules/rates/controllers/rates.controller.js
@@ -1,59 +1,223 @@
 import { logger } from "../../../utils/logger";
 import ObjLog from "../../../utils/ObjLog";
 import ratesService from "../services/rates.service";
+import authenticationPGRepository from "../../authentication/repositories/authentication.pg.repository";
+import {env,ENVIROMENTS} from '../../../utils/enviroment'
+
 const ratesController = {};
 const context = "rates Controller";
 
-ratesController.rangeRates = (req, res, next) => {
+// declaring log object
+const logConst = {
+  is_auth: null,
+  success: true,
+  failed: false,
+  ip: null,
+  country: null,
+  route: null,
+  session: null
+};
+
+ratesController.rangeRates = async (req, res, next) => {
   try {
+    // filling log object info
+    let log  = logConst;
+
+    log.is_auth = req.isAuthenticated()
+    log.ip = req.connection.remoteAddress;
+    log.route = req.method + ' ' + req.originalUrl;
+    const resp = await authenticationPGRepository.getIpInfo(req.connection.remoteAddress);
+    if (resp) log.country = resp.country_name ? resp.country_name : 'Probably Localhost';
+    if (await authenticationPGRepository.getSessionById(req.sessionID)) log.session = req.sessionID;
+
+    // protecting route in production but not in development
+    if (!req.isAuthenticated() && env.ENVIROMENT === ENVIROMENTS.PRODUCTION){
+      req.session.destroy();
+      log.success = false;
+      log.failed = true;
+      await authenticationPGRepository.insertLogMsg(log);
+      res.status(401).json({ message: "Unauthorized" });
+    }else{
+      // calling service
     logger.info(`[${context}]: Sending service to get range Rates`);
     ObjLog.log(`[${context}]: Sending service to get range Rates`);
 
-    ratesService.rangeRates(req, res, next);
+    let finalResp = await ratesService.rangeRates(req, res, next);
+    
+    if (finalResp) {
+      //logging on DB
+      log.success = finalResp.success
+      log.failed = finalResp.failed
+      await authenticationPGRepository.insertLogMsg(log);
+
+      //sendind response to FE
+      res.status(finalResp.status).json(finalResp.data);
+    }
+  }
   } catch (error) {
     next(error);
   }
 };
 
-ratesController.rateTypes = (req, res, next) => {
+ratesController.rateTypes = async (req, res, next) => {
   try {
+    // filling log object info
+    let log  = logConst;
+
+    log.is_auth = req.isAuthenticated()
+    log.ip = req.connection.remoteAddress;
+    log.route = req.method + ' ' + req.originalUrl;
+    const resp = await authenticationPGRepository.getIpInfo(req.connection.remoteAddress);
+    if (resp) log.country = resp.country_name ? resp.country_name : 'Probably Localhost';
+    if (await authenticationPGRepository.getSessionById(req.sessionID)) log.session = req.sessionID;
+
+    // protecting route in production but not in development
+    if (!req.isAuthenticated() && env.ENVIROMENT === ENVIROMENTS.PRODUCTION){
+      req.session.destroy();
+      log.success = false;
+      log.failed = true;
+      await authenticationPGRepository.insertLogMsg(log);
+      res.status(401).json({ message: "Unauthorized" });
+    }else{
+      // calling service
     logger.info(`[${context}]: Sending service to get rate Types`);
     ObjLog.log(`[${context}]: Sending service to get rate Types`);
 
-    ratesService.rateTypes(req, res, next);
+    let finalResp = await ratesService.rateTypes(req, res, next);
+      
+      if (finalResp) {
+        //logging on DB
+        log.success = finalResp.success
+        log.failed = finalResp.failed
+        await authenticationPGRepository.insertLogMsg(log);
+
+        //sendind response to FE
+        res.status(finalResp.status).json(finalResp.data);
+      }
+    }
   } catch (error) {
     next(error);
   }
 };
 
-ratesController.userRates = (req, res, next) => {
+ratesController.userRates = async (req, res, next) => {
   try {
+    // filling log object info
+    let log  = logConst;
+
+    log.is_auth = req.isAuthenticated()
+    log.ip = req.connection.remoteAddress;
+    log.route = req.method + ' ' + req.originalUrl;
+    const resp = await authenticationPGRepository.getIpInfo(req.connection.remoteAddress);
+    if (resp) log.country = resp.country_name ? resp.country_name : 'Probably Localhost';
+    if (await authenticationPGRepository.getSessionById(req.sessionID)) log.session = req.sessionID;
+
+    // protecting route in production but not in development
+    if (!req.isAuthenticated() && env.ENVIROMENT === ENVIROMENTS.PRODUCTION){
+      req.session.destroy();
+      log.success = false;
+      log.failed = true;
+      await authenticationPGRepository.insertLogMsg(log);
+      res.status(401).json({ message: "Unauthorized" });
+    }else{
+      // calling service
     logger.info(`[${context}]: Sending service to get userRates`);
     ObjLog.log(`[${context}]: Sending service to get userRates`);
 
-    ratesService.userRates(req, res, next);
+    let finalResp = await ratesService.userRates(req, res, next);
+        
+      if (finalResp) {
+        //logging on DB
+        log.success = finalResp.success
+        log.failed = finalResp.failed
+        await authenticationPGRepository.insertLogMsg(log);
+
+        //sendind response to FE
+        res.status(finalResp.status).json(finalResp.data);
+      }
+    }
   } catch (error) {
     next(error);
   }
 };
 
-ratesController.fullRates = (req, res, next) => {
+ratesController.fullRates = async (req, res, next) => {
   try {
+    // filling log object info
+    let log  = logConst;
+
+    log.is_auth = req.isAuthenticated()
+    log.ip = req.connection.remoteAddress;
+    log.route = req.method + ' ' + req.originalUrl;
+    const resp = await authenticationPGRepository.getIpInfo(req.connection.remoteAddress);
+    if (resp) log.country = resp.country_name ? resp.country_name : 'Probably Localhost';
+    if (await authenticationPGRepository.getSessionById(req.sessionID)) log.session = req.sessionID;
+
+    // protecting route in production but not in development
+    if (!req.isAuthenticated() && req.query.email_user !== 'null' && req.query.email_user && env.ENVIROMENT === ENVIROMENTS.PRODUCTION){
+      req.session.destroy();
+      log.success = false;
+      log.failed = true;
+      await authenticationPGRepository.insertLogMsg(log);
+      res.status(401).json({ message: "Unauthorized" });
+    }else{
+      // calling service
     logger.info(`[${context}]: Sending service to get fullRates`);
     ObjLog.log(`[${context}]: Sending service to get fullRates`);
 
-    ratesService.fullRates(req, res, next);
+    let finalResp = await ratesService.fullRates(req, res, next);
+          
+      if (finalResp) {
+        //logging on DB
+        log.success = finalResp.success
+        log.failed = finalResp.failed
+        await authenticationPGRepository.insertLogMsg(log);
+
+        //sendind response to FE
+        res.status(finalResp.status).json(finalResp.data);
+      }
+    }
   } catch (error) {
     next(error);
   }
 };
 
-ratesController.promo = (req, res, next) => {
+ratesController.promo = async (req, res, next) => {
   try {
+    // filling log object info
+    let log  = logConst;
+
+    log.is_auth = req.isAuthenticated()
+    log.ip = req.connection.remoteAddress;
+    log.route = req.method + ' ' + req.originalUrl;
+    const resp = await authenticationPGRepository.getIpInfo(req.connection.remoteAddress);
+    if (resp) log.country = resp.country_name ? resp.country_name : 'Probably Localhost';
+    if (await authenticationPGRepository.getSessionById(req.sessionID)) log.session = req.sessionID;
+
+    // protecting route in production but not in development
+    if (!req.isAuthenticated() && env.ENVIROMENT === ENVIROMENTS.PRODUCTION){
+      req.session.destroy();
+      log.success = false;
+      log.failed = true;
+      await authenticationPGRepository.insertLogMsg(log);
+      res.status(401).json({ message: "Unauthorized" });
+    }else{
+      // calling service
     logger.info(`[${context}]: Sending service to get promo`);
     ObjLog.log(`[${context}]: Sending service to get promo`);
 
-    ratesService.promo(req, res, next);
+    let finalResp = await ratesService.promo(req, res, next);
+            
+      if (finalResp) {
+        //logging on DB
+        log.success = finalResp.success
+        log.failed = finalResp.failed
+        await authenticationPGRepository.insertLogMsg(log);
+
+        //sendind response to FE
+        res.status(finalResp.status).json(finalResp.data);
+      }
+    }
   } catch (error) {
     next(error);
   }
