e15856938919cf14a87191383ec150c92f7a4ac1
thony <anthony81199@gmail.com>
2022-06-17 11:39:17 -0400

    cambio en logs de mail

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -5,47 +5,52 @@ import { poolSM } from "../../src/db/pg.connection";
 export default {
   sendForgotPasswordMail: async (body) => {
     try {
-      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendForgotPasswordMail`,body)
       console.log('Desde axios: ',resp.data);
+      this.log(body,resp.data)
       return resp.data;
     } catch (error) {
+      this.log(body,error)
       console.log(error);
       return error;
     }
   },
   sendSignUpMail: async (body) => {
     try {
-      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendSignUpMail`,body)
       console.log('Desde axios: ',resp.data);
+      this.log(body,resp.data)
       return resp.data;
     } catch (error) {
+      this.log(body,error)
       console.log(error);
       return error;
     }
   },
   sendAmbassadorMail: async (body) => {
     try {
-      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendAmbassadorMail`,body)
       console.log('Desde axios: ',resp.data);
+      this.log(body,resp.data)
       return resp.data;
     } catch (error) {
+      this.log(body,error)
       console.log(error);
       return error;
     }
   },
-  log: async (body) => {
+  log: async (body,response) => {
     try {
       logger.info(`[${context}]: Logging mail info in database`)
       await poolSM.query("SET SCHEMA 'sec_cust'");
       const resp = await poolSM.query(`SELECT * FROM sp_mail_logs_insert_call_to_mail_server(
-                                                                                                ($1)
+                                                                                                ($1),
+                                                                                                '${response}'
                                                                                             )`,
                                                                                             [body]);
       return resp.rows[0];
     } catch (error) {
+      this.log(body,error)
       logger.error(error);
       throw error;
     }
