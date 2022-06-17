1da29fd7db3083953109b38af5c5d55fe65a46e8
thony <anthony81199@gmail.com>
2022-06-17 11:24:23 -0400

    se guardan logs de llamadas a correos

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { env } from "../utils/enviroment";
+import { poolSM } from "../../src/db/pg.connection";
 
 export default {
   sendForgotPasswordMail: async (body) => {
     try {
+      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendForgotPasswordMail`,body)
       console.log('Desde axios: ',resp.data);
       return resp.data;
@@ -14,6 +16,7 @@ export default {
   },
   sendSignUpMail: async (body) => {
     try {
+      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendSignUpMail`,body)
       console.log('Desde axios: ',resp.data);
       return resp.data;
@@ -24,6 +27,7 @@ export default {
   },
   sendAmbassadorMail: async (body) => {
     try {
+      this.log(body)
       let resp = await axios.post(`${env.MAIL_SENDER}/sendAmbassadorMail`,body)
       console.log('Desde axios: ',resp.data);
       return resp.data;
@@ -32,4 +36,18 @@ export default {
       return error;
     }
   },
+  log: async (body) => {
+    try {
+      logger.info(`[${context}]: Logging mail info in database`)
+      await poolSM.query("SET SCHEMA 'sec_cust'");
+      const resp = await poolSM.query(`SELECT * FROM sp_mail_logs_insert_call_to_mail_server(
+                                                                                                ($1)
+                                                                                            )`,
+                                                                                            [body]);
+      return resp.rows[0];
+    } catch (error) {
+      logger.error(error);
+      throw error;
+    }
+  }
 };
