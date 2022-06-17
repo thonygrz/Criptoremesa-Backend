d443403ef5ab2a27b02fa9310f2e91225427d0bd
thony <anthony81199@gmail.com>
2022-06-17 11:47:49 -0400

    cambio en mail logs

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -2,6 +2,23 @@ import axios from 'axios';
 import { env } from "../utils/enviroment";
 import { poolSM } from "../../src/db/pg.connection";
 
+async function log(body,response) {
+  try {
+    logger.info(`[${context}]: Logging mail info in database`)
+    await poolSM.query("SET SCHEMA 'sec_cust'");
+    const resp = await poolSM.query(`SELECT * FROM sp_mail_logs_insert_call_to_mail_server(
+                                                                                              ($1),
+                                                                                              '${response}'
+                                                                                          )`,
+                                                                                          [body]);
+    return resp.rows[0];
+  } catch (error) {
+    log(body,error)
+    logger.error(error);
+    throw error;
+  }
+}
+
 export default {
   sendForgotPasswordMail: async (body) => {
     try {
@@ -37,22 +54,6 @@ export default {
       log(body,error)
       console.log(error);
       return error;
-    }
-  },
-  log: async (body,response) => {
-    try {
-      logger.info(`[${context}]: Logging mail info in database`)
-      await poolSM.query("SET SCHEMA 'sec_cust'");
-      const resp = await poolSM.query(`SELECT * FROM sp_mail_logs_insert_call_to_mail_server(
-                                                                                                ($1),
-                                                                                                '${response}'
-                                                                                            )`,
-                                                                                            [body]);
-      return resp.rows[0];
-    } catch (error) {
-      log(body,error)
-      logger.error(error);
-      throw error;
     }
   }
 };
