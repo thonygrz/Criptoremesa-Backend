588ec31479113a3b5c6a85cf018edc8fee08beec
thony <anthony81199@gmail.com>
2022-06-17 12:12:55 -0400

    cambio en mail logs

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -5,11 +5,12 @@ import { logger } from "../utils/logger";
 
 const context = "MAIL MODULE";
 
-async function log(url,body,response) {
+async function log(email_user,url,body,response) {
   try {
     logger.info(`[${context}]: Logging mail info in database`)
     await poolSM.query("SET SCHEMA 'sec_cust'");
     const resp = await poolSM.query(`SELECT * FROM sp_mail_logs_insert_call_to_mail_server(
+                                                                                              '${email_user}',
                                                                                               '${url}',
                                                                                               ($1),
                                                                                               '${response}'
@@ -17,7 +18,7 @@ async function log(url,body,response) {
                                                                                           [body]);
     return resp.rows[0];
   } catch (error) {
-    log(url,body,error)
+    log(body.email_user,url,body,error)
     logger.error(error);
     throw error;
   }
@@ -29,10 +30,10 @@ export default {
     try {
       let resp = await axios.post(url,body)
       console.log('Desde axios: ',resp.data);
-      log(url,body,resp.data)
+      log(body.email_user,url,body,resp.data)
       return resp.data;
     } catch (error) {
-      log(url,body,error)
+      log(body.email_user,url,body,error)
       console.log(error);
       return error;
     }
@@ -42,10 +43,10 @@ export default {
     try {
       let resp = await axios.post(url,body)
       console.log('Desde axios: ',resp.data);
-      log(url,body,resp.data)
+      log(body.email_user,url,body,resp.data)
       return resp.data;
     } catch (error) {
-      log(url,body,error)
+      log(body.email_user,url,body,error)
       console.log(error);
       return error;
     }
@@ -55,10 +56,10 @@ export default {
     try {
       let resp = await axios.post(url,body)
       console.log('Desde axios: ',resp.data);
-      log(url,body,resp.data)
+      log(body.email_user,url,body,resp.data)
       return resp.data;
     } catch (error) {
-      log(url,body,error)
+      log(body.email_user,url,body,error)
       console.log(error);
       return error;
     }
