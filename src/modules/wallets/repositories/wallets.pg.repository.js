bbe3b835a1eb9a19790167c9123351b9802520b6
thony <anthony81199@gmail.com>
2022-07-22 14:41:27 -0400

    ep para obtener wallets

---
+++ src/modules/wallets/repositories/wallets.pg.repository.js
@@ -0,0 +1,30 @@
+import { poolSM } from "../../../db/pg.connection";
+import { logger } from "../../../utils/logger";
+import ObjLog from "../../../utils/ObjLog";
+
+const walletsRepository = {};
+const context = "Wallets PG Repository";
+
+walletsRepository.getWallets = async (onlyCompany) => {
+  try {
+    logger.info(`[${context}]: Getting wallets from db`);
+    ObjLog.log(`[${context}]: Getting wallets from db`);
+    await poolSM.query("SET SCHEMA 'msg_app'");
+
+    onlyCompany = onlyCompany === 'null' ? null :  onlyCompany
+
+    const resp = await poolSM.query(
+      `select * from msg_app.sp_get_wallets(
+                                                  ${onlyCompany}
+                                                  )`
+    );
+    if (resp.rows[0].sp_get_wallets)
+      return resp.rows[0].sp_get_wallets;
+    else 
+      []
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default walletsRepository;
