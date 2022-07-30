1e25043738d27a0961fcd125d6d8cda032b40bd9
thony <anthony81199@gmail.com>
2022-07-30 09:42:42 -0400

    no se leen imágenes en getExchangesByUser

--- src/modules/exchanges/services/exchanges.service.js
+++ src/modules/exchanges/services/exchanges.service.js
@@ -415,15 +415,18 @@ exchangesService.getExchangesByUser = async (req, res, next) => {
       clearTimeout(reply)
     });
     let data = await exchangesRepository.getExchangesByUser(req.query);
-    data.forEach(el=>{
-      if (el.captures){
-        el.captures.forEach(capture=>{
-          capture.content = fs.readFileSync(
-            capture.content
-          );
-        })
-      }
-    })
+
+    // se leen las imágenes
+
+    // data.forEach(el=>{
+    //   if (el.captures){
+    //     el.captures.forEach(capture=>{
+    //       capture.content = fs.readFileSync(
+    //         capture.content
+    //       );
+    //     })
+    //   }
+    // })
 
     return {
       data,
