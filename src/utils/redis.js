b39d60a770b663192f308f4a273c7eb932b4e32c
thony <anthony81199@gmail.com>
2022-08-04 13:27:49 -0400

    cert y redis

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,8 +1,8 @@
 import redis from 'redis';
 
 const client = redis.createClient({
-    host: '127.0.0.1',
-    port: 6379,
+    host: '172.31.60.8',
+    port: 6389,
     password: '/r/Ik+eWEBKg38uTAKOKhtqFPPuxJG/1/trbkPi1attciTtbopSonLNIthkIxUJjDNJApc9jpNnctmsJ',
     db: 1,
     read_timeout: 60,
