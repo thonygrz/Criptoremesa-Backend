6f7ce27b462520f1c6ac17151ba1af794360887c
Anthony <anthony81199@gmail.com>
2022-09-09 16:29:00 +0000

    se agrega el cálculo del monto de las remesas en moneda local a una función lambda

--- src/utils/awsLambda.js
+++ src/utils/awsLambda.js
@@ -3,24 +3,23 @@ import axios from 'axios'
 import { logger } from './logger'
 
 let awsLambda = {}
-const baseURL = env.AWS_LAMBDA
 
-async function request(method,endpoint,queryString,body) {
-    logger.silly('REQUEST: '+ baseURL + '/' + endpoint + (queryString ? '?' + queryString : ''))
+async function request(method,baseURL,queryString,body) {
+    logger.silly('REQUEST: '+ baseURL + (queryString ? '?' + queryString : ''))
     switch (method) {
         case 'POST':
-            return (await axios.post(baseURL + '/' + endpoint + (queryString ? '?' + queryString : ''),body)).data
+            return (await axios.post(baseURL + (queryString ? '?' + queryString : ''),body)).data
         case 'GET':
             return (await axios.get(baseURL + '/' + endpoint + '?' + queryString + '&signature=' + signature)).data
     }
 } 
 
 awsLambda.comparePasswords = async function (pass1,pass2) {
-    return await request('POST','comparePassword',null,{pass1,pass2})
+    return await request('POST',env.AWS_LAMBDA_COMPARE_PASSWORDS,null,{pass1,pass2})
 }
 
 awsLambda.getLocalAmountLimit = async function (currentManualRate,fullRateFromAPI) {
-    return await request('POST','getLocalAmountLimit',null,{currentManualRate,fullRateFromAPI})
+    return await request('POST',env.AWS_LAMBDA_GET_LOCAL_AMOUNT_LIMIT,null,{currentManualRate,fullRateFromAPI})
 }
 
 export default awsLambda;
